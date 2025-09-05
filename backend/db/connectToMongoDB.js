0dce04d2a86fec6270b40ce6fe18a1701835101c
unknown <Abhay Mehta@MyPc.com>
2025-09-05 12:10:28 +0530

    first commit

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,36 @@
+// import mongoose from "mongoose";
+
+// const connectToMongoDB = async () => {
+// 	try {
+// 		await mongoose.connect(process.env.MONGO_DB_URI);
+// 		console.log("Connected to MongoDB");
+// 	} catch (error) {
+// 		console.log("Error connecting to MongoDB", error.message);
+// 	}
+// };
+
+// export default connectToMongoDB;
+
+
+// /////  npm start
+
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  try {
+    // 👇 paste your Compass/Atlas URI here
+    const mongoURI = "mongodb+srv://abhaymehta202001_db_user:zzsYvBYK0qish3v9@chat.uieue1w.mongodb.net/";
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅ MongoDB Connected...");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectToMongoDB;
